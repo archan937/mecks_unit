dbd29f9022a4d04d0801c6ba9d52df3db89bcfc7
Paul Engel <pm_engel@icloud.com>
2019-01-11 00:14:14 +0100

    Add async tests

--- test/mecks_unit_test.exs
+++ test/mecks_unit_test.exs
@@ -1,4 +1,39 @@
 defmodule MecksUnitTest do
-  use ExUnit.Case
-  doctest MecksUnit
+  use ExUnit.Case, async: true
+
+  import MecksUnit.Case
+
+  mocked_test :mocking_demo, "using mocked module functions" do
+    task =
+      Task.async(fn ->
+        assert "Engel" == String.trim("  Paul  ")
+        assert "Bar" == String.trim("  Foo  ", "!")
+        assert "  Surprise!  " == String.trim("  Paul  ", "!")
+        assert "MecksUnit" == String.trim("  MecksUnit  ")
+        assert "Paul Engel" == String.trim("  Paul Engel  ", " ")
+        assert [1, 2, 3, 4] == List.wrap(:foo)
+        assert [] == List.wrap(nil)
+        assert [:bar] == List.wrap(:bar)
+        assert [:foo, :bar] == List.wrap([:foo, :bar])
+      end)
+
+    Task.await(task)
+  end
+
+  test "using the original module functions" do
+    task =
+      Task.async(fn ->
+        assert "Paul" == String.trim("  Paul  ")
+        assert "  Foo  " == String.trim("  Foo  ", "!")
+        assert "  Paul  " == String.trim("  Paul  ", "!")
+        assert "MecksUnit" == String.trim("  MecksUnit  ")
+        assert "Paul Engel" == String.trim("  Paul Engel  ", " ")
+        assert [:foo] == List.wrap(:foo)
+        assert [] == List.wrap(nil)
+        assert [:bar] == List.wrap(:bar)
+        assert [:foo, :bar] == List.wrap([:foo, :bar])
+      end)
+
+    Task.await(task)
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,32 @@
 ExUnit.start()
+
+import MecksUnit
+
+defmock String, [
+  mocking_demo: [
+    trim: fn
+      "  Paul  " ->
+        "Engel"
+    end,
+    trim: fn
+      "  Foo  ", "!" ->
+        "Bar"
+      _, to_trim ->
+        case to_trim do
+          "!" ->
+            {:passthrough, ["  Surprise!  !!!!", "!"]}
+          _ ->
+            :passthrough
+        end
+    end
+  ]
+]
+
+defmock List, [
+  mocking_demo: [
+    wrap: fn
+      :foo ->
+        [1, 2, 3, 4]
+    end
+  ]
+]
