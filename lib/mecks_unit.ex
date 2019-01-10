c1870409d806ec6a3b392cf69a637b31f8a2f606
Paul Engel <pm_engel@icloud.com>
2019-01-10 10:07:10 +0100

    Initial commit

---
+++ lib/mecks_unit.ex
@@ -0,0 +1,18 @@
+defmodule MecksUnit do
+  @moduledoc """
+  Documentation for MecksUnit.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MecksUnit.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mecks_unit_test.exs
@@ -0,0 +1,8 @@
+defmodule MecksUnitTest do
+  use ExUnit.Case
+  doctest MecksUnit
+
+  test "greets the world" do
+    assert MecksUnit.hello() == :world
+  end
+end
