3f9503968e7e533036594d83941cac7a1761405b
Paul Engel <pm_engel@icloud.com>
2019-01-21 22:41:24 +0100

    Trivial correction of foo_test.exs

--- test/mecks_unit/foo_test.exs
+++ test/mecks_unit/foo_test.exs
@@ -3,14 +3,14 @@ defmodule MecksUnit.FooTest do
   use MecksUnit.Case
 
   defmock List do
-    def wrap(:foo_test), do: ~w(MecksUnit Bar Test)
+    def wrap(:foo_test), do: ~w(MecksUnit Foo Test)
   end
 
   mocked_test "parallel compiling" do
     task =
       Task.async(fn ->
         assert [:foo, :bar] == List.wrap([:foo, :bar])
-        assert ~w(MecksUnit Bar Test) == List.wrap(:foo_test)
+        assert ~w(MecksUnit Foo Test) == List.wrap(:foo_test)
         assert called(List.wrap(:foo_test))
       end)
 
